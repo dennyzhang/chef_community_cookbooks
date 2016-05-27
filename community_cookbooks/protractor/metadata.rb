4f2d746a3f593398e78ddbd24de0ccdcfceb9283
dennyzhang <denny@dennyzhang.com>
2016-05-27 18:14:44 +0800

    protractor use apt update first

--- community_cookbooks/protractor/metadata.rb
+++ community_cookbooks/protractor/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'denny@dennyzhang.com'
 license 'All rights reserved'
 description 'Chef cookbook to install and setup protractor'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.9'
+version '0.2.0'
 
 supports 'arch'
 supports 'centos'
