cf3e657cf4e911bc565d85d9167c9492a274cb31
dennyzhang <denny@dennyzhang.com>
2016-05-30 18:17:42 +0800

    ssh community cookbook: disable root login

--- cookbooks/general_security/attributes/default.rb
+++ cookbooks/general_security/attributes/default.rb
@@ -1,3 +1,3 @@
 # -*- encoding: utf-8 -*-
 default['general_security']['ssh_disable_passwd_login'] = 'true'
-default['general_security']['ssh_disable_root_login'] = 'true'
+default['general_security']['ssh_disable_root_login'] = 'false'
--- cookbooks/general_security/recipes/sshd.rb
+++ cookbooks/general_security/recipes/sshd.rb
@@ -7,8 +7,23 @@
 # Apache License, Version 2.0
 #
 
-# TODO: SSH disable password login
+# sed -i 's/PermitRootLogin .*/PermitRootLogin without-password/' /etc/ssh/sshd_config
+if node['general_security']['ssh_disable_passwd_login'] == 'true'
+  execute 'SSH disable password login' do
+    command 'sed -i "s/PasswordAuthentication yes/PasswordAuthentication no/g" '\
+            '/etc/ssh/sshd_config'
+    action :run
+    only_if 'grep "PasswordAuthentication yes" /etc/ssh/sshd_config'
+  end
+end
 
-# TODO: SSH disable root login
+if node['general_security']['ssh_disable_root_login'] == 'true'
+  execute 'SSH disable root login' do
+    command 'sed -i "s/PermitRootLogin yes/PermitRootLogin without-password/g" '\
+            '/etc/ssh/sshd_config'
+    action :run
+    only_if 'grep "PermitRootLogin yes" /etc/ssh/sshd_config'
+  end
+end
 
 # TODO: SSH configure gateway
