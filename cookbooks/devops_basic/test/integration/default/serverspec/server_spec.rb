459d27fcdd85c9914dc52160c23a1aa32578befd
dennyzhang <denny@dennyzhang.com>
2016-06-16 10:12:49 +0800

    rubocop fix

--- cookbooks/devops_basic/test/integration/default/serverspec/server_spec.rb
+++ cookbooks/devops_basic/test/integration/default/serverspec/server_spec.rb
@@ -5,7 +5,7 @@ require 'serverspec'
 set :backend, :exec
 
 %w(/usr/local/bin/enforce_all_nagios_check.sh
- /opt/devops/bin/wait_for.sh).each do |x|
+   /opt/devops/bin/wait_for.sh).each do |x|
   describe file(x) do
     it { should be_file }
   end
