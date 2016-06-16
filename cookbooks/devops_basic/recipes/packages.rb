7656231fbe90cd1f09b8247ac64912ce0276e254
dennyzhang <denny@dennyzhang.com>
2016-06-16 10:01:52 +0800

    install packages

--- cookbooks/devops_basic/recipes/packages.rb
+++ cookbooks/devops_basic/recipes/packages.rb
@@ -8,3 +8,17 @@
 # Apache License, Version 2.0
 #
 include_recipe 'apt::default'
+
+%w(lsof inotify-tools telnet tar tree vim).each do |x|
+  package x do
+    action :install
+    not_if "dpkg -l #{x} | grep -E '^ii'"
+  end
+end
+
+%w(curl git tmux syslinux bc python-pip).each do |x|
+  package x do
+    action :install
+    not_if "dpkg -l #{x} | grep -E '^ii'"
+  end
+end
