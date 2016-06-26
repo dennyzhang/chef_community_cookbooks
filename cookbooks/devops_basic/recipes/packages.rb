3725e3b5cf2aec5849fbedb0cc944ced12849cd4
dennyzhang <denny@dennyzhang.com>
2016-06-27 07:37:08 +0800

    enable people to configure what packages to install

--- cookbooks/devops_basic/recipes/packages.rb
+++ cookbooks/devops_basic/recipes/packages.rb
@@ -16,23 +16,3 @@ node['devops_basic']['package_list'].each do |x|
     not_if "dpkg -l #{x} | grep -E '^ii'"
   end
 end
-
-################################################################################
-if node['devops_basic']['whether_install_justniffer'] == 'true'
-  # whether install justniffer
-  apt_repository 'ruby2.1-repo' do
-    uri 'ppa:oreste-notelli/ppa'
-    distribution node['lsb']['codename']
-    key 'C3173AA6'
-    keyserver 'keyserver.ubuntu.com'
-    retries 3
-    retry_delay 3
-    not_if { ::File.exist?('/etc/apt/sources.list.d/ruby2.1-repo.list') }
-  end
-
-  package 'justniffer' do
-    action :install
-    not_if "dpkg -l #{x} | grep -E '^ii'"
-  end
-end
-################################################################################
