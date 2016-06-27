49b4452fe769a6eddf390d477ca46fe5f1e3d78e
dennyzhang <denny@dennyzhang.com>
2016-06-27 16:48:06 +0800

    update justniffer version

--- cookbooks/justniffer/metadata.rb
+++ cookbooks/justniffer/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'denny@dennyzhang.com'
 license 'All rights reserved'
 description 'Track all outgoing http requests by justniffer'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.1'
+version '0.1.2'
 
 supports 'ubuntu'
 
