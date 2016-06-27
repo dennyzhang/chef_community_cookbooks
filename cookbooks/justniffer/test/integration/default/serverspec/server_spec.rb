bfc057a67c02a9dd6d568633d1b27e548cc7fbee
dennyzhang <denny@dennyzhang.com>
2016-06-27 16:44:57 +0800

    serverspec to test justniffer

--- cookbooks/justniffer/test/integration/default/serverspec/server_spec.rb
+++ cookbooks/justniffer/test/integration/default/serverspec/server_spec.rb
@@ -9,3 +9,11 @@ set :backend, :exec
     it { should be_installed }
   end
 end
+
+describe command('pgrep justniffer') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe file('/root/justniffer.log') do
+  it { should be_file }
+end
