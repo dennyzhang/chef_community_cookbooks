27c338ad2183a805c0d933abc810fa9b22223a1a
dennyzhang <denny@dennyzhang.com>
2016-07-08 11:11:19 +0800

    update serverspec location

--- cookbooks/devops_library/recipes/install_serverspec.rb
+++ cookbooks/devops_library/recipes/install_serverspec.rb
@@ -7,15 +7,8 @@
 #
 # Apache License, Version 2.0
 #
-directory '/opt/serverspec' do
-  owner 'root'
-  group 'root'
-  mode 00755
-  action :create
-end
-
 %w(jenkins_helper.rb common_library.rb db_helper.rb general_helper.rb).each do |x|
-  cookbook_file "/opt/serverspec/#{x}" do
+  cookbook_file "/opt/#{x}" do
     source "devops_public/serverspec/#{x}"
   end
 end
