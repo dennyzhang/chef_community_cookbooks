53c1fc39e7a951a2c90259784e753ad623b67f53
dennyzhang <denny@dennyzhang.com>
2016-07-08 13:20:20 +0800

    update version

--- cookbooks/devops_library/metadata.rb
+++ cookbooks/devops_library/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'denny@dennyzhang.com'
 license 'All rights reserved'
 description 'Common DevOps Scripts'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.1'
+version '0.0.2'
 
 supports 'ubuntu'
 
