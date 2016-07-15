dc05eae15379ca7aefacc2cc8fa1b6e37f556f21
dennyzhang <denny@dennyzhang.com>
2016-07-16 06:40:17 +0800

    keep file in sync

--- cookbooks/devops_library/files/default/devops_public/bash/dump_db_summary/dump_db_summary.sh
+++ cookbooks/devops_library/files/default/devops_public/bash/dump_db_summary/dump_db_summary.sh
@@ -8,9 +8,18 @@
 ## Author : Denny <denny@dennyzhang.com>
 ## Description :
 ## Sample:
+##          /opt/devops/dump_db_summary/cfg_dir/couchbase.cfg -> dump_couchbase_summary
+##             export server_ip=192.168.0.3
+##             export tcp_port=8091
+##             export cb_username=Administrator
+##             export cb_passwd=MyDBPassword1
+##
+##          /opt/devops/dump_db_summary/cfg_dir/elasticsearch.cfg -> dump_elasticsearch_summary
+##             export server_ip=192.168.0.4
+##             export tcp_port=9200
 ## --
 ## Created : <2016-06-04>
-## Updated: Time-stamp: <2016-06-24 15:52:59>
+## Updated: Time-stamp: <2016-07-15 14:26:11>
 ##-------------------------------------------------------------------
 . /etc/profile
 
@@ -18,48 +27,80 @@
 # Plugin Function
 function dump_couchbase_summary() {
     local cfg_file=${1?}
-    local output_type=${2?}
-    local output_data_file=${3?}
+    local output_file_prefix=${2?}
+
+    logstash_postfix="_logstash.txt"
+    stdout_output_file="${output_file_prefix}.out"
     source "$cfg_file"
+
+    tmp_data_file="/tmp/dump_couchbase_$$.log"
     # Get parameters from $cfg_file:
     #    server_ip, tcp_port, cb_username, cb_password
-    if [ "$output_type" = "json" ]; then
-        echo "Call http://${server_ip}:${tcp_port}/pools/default/buckets"
-        curl -u "${cb_username}:${cb_passwd}" "http://${server_ip}:${tcp_port}/pools/default/buckets" \
-            | python -m json.tool > "$output_data_file"
+    echo "Call http://${server_ip}:${tcp_port}/pools/default/buckets"
+    curl -u "${cb_username}:${cb_passwd}" "http://${server_ip}:${tcp_port}/pools/default/buckets" \
+        | python -m json.tool > "$tmp_data_file"
 
-        # parse json to get the summary
-        output=$(python -c "import sys,json
+    # parse json to get the summary
+    output=$(python -c "import sys,json
 list = json.load(sys.stdin)
-list = map(lambda x: '%s: %s' % (x['name'], x['basicStats']), list)
-print json.dumps(list)" < "$output_data_file")
-        echo "$output" | python -m json.tool > "$output_data_file"
-        # TODO: need to beautify json output
-    fi
+print 'bucket\tdiskUsed\tmemUsed\tdiskFetches\tquotaPercentUsed\topsPerSec\tdataUsed\titemCount'
+list = map(lambda x: '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % (str(x['name']), str(x['basicStats']['diskUsed']), str(x['basicStats']['memUsed']), str(x['basicStats']['diskFetches']), str(x['basicStats']['quotaPercentUsed']), str(x['basicStats']['opsPerSec']), str(x['basicStats']['dataUsed']), str(x['basicStats']['itemCount'])), list)
+print '\n'.join(list)" < "$tmp_data_file")
+    rm -rf "$tmp_data_file"
+    echo "$output" > "$stdout_output_file"
+
+    # output columns: bucket diskUsed memUsed diskFetches quotaPercentUsed opsPerSec dataUsed itemCount
+    IFS=$'\n'
+    grep -v "^bucket"  < "$stdout_output_file" | while IFS= read -r line
+    do
+        unset IFS
+        item_name=$(echo "$line" | awk -F"\t" '{print $1}')
+        prop_value=$(echo "$line" | awk -F"\t" '{print $8}')
+        # sample output: echo "[11/Jul/2016:14:10:45 +0000] mdm-master CBItemNum 20" >> /var/log/data_report.log
+        insert_elk_entry "$item_name" "CBItemNum" "$prop_value" "${output_file_prefix}${logstash_postfix}"
+    done
 }
 
 function dump_elasticsearch_summary() {
     local cfg_file=${1?}
-    local output_type=${2?}
-    local output_data_file=${3?}
+    local output_file_prefix=${2?}
 
+    logstash_postfix="_logstash.txt"
+    stdout_output_file="${output_file_prefix}.out"
     source "$cfg_file"
-    # Get parameters from $cfg_file:
-    #    server_ip, tcp_port
-    if [ "$output_type" = "json" ]; then
-        # TODO: Change to json
-        echo "Call http://${server_ip}:${tcp_port}/_cat/shards?v"
-        curl "http://${server_ip}:${tcp_port}/_cat/shards?v" \
-             > "$output_data_file"
-    fi
+
+    # Get parameters from $cfg_file: server_ip, tcp_port
+    echo "Call http://${server_ip}:${tcp_port}/_cat/shards?v"
+    curl "http://${server_ip}:${tcp_port}/_cat/shards?v" > "$stdout_output_file"
+
+    # output columns: index shard prirep state docs store ip node
+    IFS=$'\n'
+    grep -v "^index " < "$stdout_output_file" | grep "  p  " | while IFS= read -r line
+    do
+        unset IFS
+        item_name=$(echo "$line" | awk -F' ' '{print $1}')
+        prop_value=$(echo "$line" | awk -F' ' '{print $5}')
+        # store=$(echo "$line" | awk -F' ' '{print $6}')
+        # sample output: echo "[11/Jul/2016:14:10:45 +0000] master-index-8cd6e43115 ESItemNum 200" >> /var/log/data_report.log
+        insert_elk_entry "$item_name" "ESItemNum" "$prop_value" "${output_file_prefix}${logstash_postfix}"
+    done
+}
+
+function insert_elk_entry() {
+    local item_name=${1?}
+    local property_name=${2?}
+    local property_value=${3?}
+    local data_file=${4?}
+
+    LANG=en_US
+    datetime_utc=$(date -u +['%d/%h/%Y:%H:%M:%S +0000'])
+    echo "$datetime_utc $item_name $property_name $property_value" >> "$data_file"
 }
 
 ################################################################################
 stdout_show_data_out=${1:-"false"}
 cfg_dir=${2:-"/opt/devops/dump_db_summary/cfg_dir"}
 data_out_dir=${3:-"/opt/devops/dump_db_summary/data_out"}
-# support string/json
-output_type=${4:-"json"}
 
 [ -d "$cfg_dir" ] || mkdir -p "$cfg_dir"
 [ -d "$data_out_dir" ] || mkdir -p "$data_out_dir"
@@ -68,17 +109,16 @@ cd "$cfg_dir"
 for f in *.cfg; do
     if [ -f "$f" ]; then
         db_name=${f%%.cfg}
-        # Sample: $cfg_dir/mongodb.cfg -> dump_mongodb_summary mongodb.cfg
         fun_name="dump_${db_name}_summary"
-        command="$fun_name $f $output_type $data_out_dir/${db_name}.${output_type}"
+        command="$fun_name $f $data_out_dir/${db_name}"
         echo "Run function: $command"
-        $command
+        "dump_${db_name}_summary" "$f" "$data_out_dir/${db_name}"
     fi
 done
 
 if [ "$stdout_show_data_out" = "true" ]; then
     cd "$data_out_dir"
-    for f in *.${output_type}; do
+    for f in *.out; do
         if [ -f "$f" ]; then
             db_name=${f%%.*}
             echo "Dump $db_name data summary: $data_out_dir/$f"
