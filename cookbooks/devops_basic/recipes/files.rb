138f0a773192be61870ee865767c9888ea59ee28
dennyzhang <denny@dennyzhang.com>
2016-10-27 18:00:00 +0800

    revert change

--- cookbooks/devops_basic/recipes/files.rb
+++ cookbooks/devops_basic/recipes/files.rb
@@ -1,9 +1,85 @@
-elasticsearch_configure 'elasticsearch' do
-  allocated_memory node['my_elasticsearch']['allocated_memory']
-  configuration (
-    'cluster.name' => node['my_elasticsearch']['cluster_name'],
-    'discovery.zen.ping.multicast.enabled' => \
-    node['my_elasticsearch']['ping_timeout'],
-    'network.host' => es_network_host
-  )
+# -*- encoding: utf-8 -*-
+#
+# Cookbook Name:: devops_basic
+# Recipe:: files
+#
+# Copyright 2015, http://DennyZhang.com
+#
+# Apache License, Version 2.0
+#
+%w(
+  /usr/local/var /usr/local/var/run /var/chef /var/chef/cache
+  /opt/devops /opt/devops/bin /data
+).each do |x|
+  directory x do
+    owner 'root'
+    group 'root'
+    mode 0o755
+    action :create
+  end
+end
+
+%w(/data/backup).each do |x|
+  directory x do
+    owner 'root'
+    group 'root'
+    mode 0o777
+    action :create
+  end
+end
+
+cookbook_file '/opt/devops/bin/enforce_all_nagios_check.sh' do
+  source 'devops_public/bash/enforce_all_nagios_check/enforce_all_nagios_check.sh'
+  owner 'root'
+  group 'root'
+  mode 0o755
+  cookbook 'devops_library'
+end
+
+cookbook_file '/opt/devops/bin/create_loop_device.sh' do
+  source 'devops_public/bash/create_loop_device.sh'
+  owner 'root'
+  group 'root'
+  mode 0o755
+  cookbook 'devops_library'
+end
+
+cookbook_file '/opt/devops/bin/disable_oom.sh' do
+  source 'devops_public/bash/disable_oom.sh'
+  owner 'root'
+  group 'root'
+  mode 0o755
+  cookbook 'devops_library'
+end
+
+cookbook_file '/opt/devops/bin/free_cache.sh' do
+  source 'devops_public/bash/free_cache.sh'
+  owner 'root'
+  group 'root'
+  mode 0o755
+  cookbook 'devops_library'
+end
+
+cookbook_file '/opt/devops/bin/manage_all_services.sh' do
+  source 'devops_public/bash/manage_all_services/manage_all_services.sh'
+  owner 'root'
+  group 'root'
+  mode 0o755
+  cookbook 'devops_library'
+end
+
+cookbook_file '/opt/devops/bin/wait_for.sh' do
+  source 'devops_public/bash/wait_for/wait_for.sh'
+  owner 'root'
+  group 'root'
+  mode 0o755
+  cookbook 'devops_library'
+end
+
+cookbook_file '/opt/devops/bin/docker_destroy.sh' do
+  source 'devops_public/bash/docker_facility/docker_destroy.sh'
+  owner 'root'
+  group 'root'
+  mode 0o755
+  cookbook 'devops_library'
 end
--- cookbooks/devops_basic/test/integration/default/serverspec/server_spec.rb
+++ cookbooks/devops_basic/test/integration/default/serverspec/server_spec.rb
@@ -4,16 +4,15 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-def run_check_jenkins_job(jenkins_run_cmd, jenkins_check_cmd, \
-                          job_name, parameters)
-  # Run jenkins jobs once and verify the job status
-  describe command("#{jenkins_run_cmd} #{job_name} -w #{parameters}") do
-    its(:stdout) { should contain 'Started ' }
-    its(:exit_status) { should eq 0 }
+%w(/opt/devops/bin/enforce_all_nagios_check.sh
+   /opt/devops/bin/wait_for.sh).each do |x|
+  describe file(x) do
+    it { should be_file }
   end
+end
 
-  describe command("#{jenkins_check_cmd} #{job_name}") do
-    its(:stdout) { should contain 'Jenkins job success: ' }
-    its(:exit_status) { should eq 0 }
+%w(lsof curl tmux).each do |x|
+  describe package(x) do
+    it { should be_installed }
   end
 end
