ccada65ad9a80e019d13e0d574a1e09d4be70a89
dennyzhang <denny@dennyzhang.com>
2016-12-24 00:18:21 +0800

    improve docstring

--- cookbooks/devops_library/libraries/pre_check_helper.rb
+++ cookbooks/devops_library/libraries/pre_check_helper.rb
@@ -3,15 +3,20 @@ module PreCheck
 
   # Precheck for input parameters and infrastructure layer
   module HelperInputFormat
-    # Parmaeter checks for input format
+    # Parameter checks for input format
 
     def check_string_not_empty(value, extra_msg)
+      # If value is empty, log error message and quit
+      #   Sample: check_string_not_empty("21", "ssh_port is empty")
       return unless value == ''
       err_msg = "Error: parameter is empty. #{extra_msg}"
       Chef::Application.fatal!(err_msg)
     end
 
     def check_ip_format(ip_address, extra_msg = '')
+      # If ip_address is not valid ip address, log and quit
+      #   Sample: check_ip_format("192.168.1.123a", "nagios_ip is invalid")
+      #   Sample: check_ip_format("192.168.1.123a")
       block = /\d{,2}|1\d{2}|2[0-4]\d|25[0-5]/
       re = /\A#{block}\.#{block}\.#{block}\.#{block}\z/
 
@@ -23,6 +28,7 @@ module PreCheck
     end
 
     def check_ip_list_format(ip_address_list)
+      #   Sample: check_ip_format(['192.168.1.122', '192.168.1.123'])
       # TODO: improve the error message
       # extra_msg = "#{extra_msg} is not a valid ip list" if extra_msg != ''
 
@@ -31,8 +37,8 @@ module PreCheck
       end
     end
 
-    # make sure node name is valid. Doesn't have special characters like ',', ';', etc
     def check_nodename_format(node_name, extra_msg = '')
+      # Make sure node name is valid. Doesn't have special characters like ',', ';', etc
       return unless !node_name.index(',').nil? || !node_name.index(';').nil?
       err_msg = "Error: invalid node name #{node_name}.#{extra_msg}"
       Chef::Application.fatal!(err_msg)
@@ -81,11 +87,9 @@ module PreCheck
     end
 
     def check_os_version(supported_os_list, current_os_version)
-      supported_os_list.each do |os_version|
-        return unless os_version != current_os_version
-        Chef::Application.fatal!("Current OS version is #{current_os_version}. " \
-                                 "Supported OS versions: #{supported_os_list.join(',')}")
-      end
+      return unless supported_os_list.include? current_os_version
+      Chef::Application.fatal!("Current OS version is #{current_os_version}. " \
+                               "Supported OS versions: #{supported_os_list.join(',')}")
     end
   end
 
@@ -106,6 +110,5 @@ module PreCheck
     def check_port_connect(ip_address, tcp_port)
       # TODO: to be implemented
     end
-
   end
 end
