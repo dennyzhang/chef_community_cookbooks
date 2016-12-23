741c724678eb92b463b9d559d2945483b774530c
dennyzhang <denny@dennyzhang.com>
2016-12-24 00:24:02 +0800

    improve docstring

--- cookbooks/devops_library/libraries/pre_check_helper.rb
+++ cookbooks/devops_library/libraries/pre_check_helper.rb
@@ -39,12 +39,15 @@ module PreCheck
 
     def check_nodename_format(node_name, extra_msg = '')
       # Make sure node name is valid. Doesn't have special characters like ',', ';', etc
+      #   Sample: check_nodename_format('node1', 'nagios server name is invalid')
       return unless !node_name.index(',').nil? || !node_name.index(';').nil?
       err_msg = "Error: invalid node name #{node_name}.#{extra_msg}"
       Chef::Application.fatal!(err_msg)
     end
 
     def check_nodename_list_format(node_name_list)
+      #   Sample: check_nodename_format(['node1', '192.168.1.3'])
+
       # TODO: improve the error message
       # extra_msg = "#{extra_msg} is not a valid ip list" if extra_msg != ''
 
@@ -67,12 +70,15 @@ module PreCheck
   module HelperInfraCheck
     def check_capacity_cpu(min_cpu_count, current_cpu_count)
       # Check enough CPU cores
+      #   Sample: check_capacity_cpu(4, 6)
       return unless current_cpu_count < min_cpu_count.to_i
       Chef::Application.fatal!('Low cpu: the machine should have at least ' + \
                                min_cpu_count + ' cpu cores')
     end
 
     def check_capacity_ram(min_ram_gb, current_ram_gb)
+      # Check enough RAM
+      #   Sample: check_capacity_ram(8, 12)
       return unless current_ram_gb < min_ram_gb.to_f
       Chef::Application.fatal!('Low memory: the machine should have at least ' \
                                "#{min_ram_gb} GB RAM," \
@@ -80,6 +86,8 @@ module PreCheck
     end
 
     def check_hostname(hostname, extra_msg = '')
+      # Make sure hostname has been reconfigured
+      # TDOO: improve the implementation
       return unless hostname == 'ubuntu'
       err_msg = "Error: hostname is #{hostname}. "\
                 "Please check whether your hostname is properly configured.#{extra_msg}"
@@ -87,6 +95,7 @@ module PreCheck
     end
 
     def check_os_version(supported_os_list, current_os_version)
+      #   Sample: check_os_version(['ubuntu-14.04', 'centos-6.5'], 'ubuntu-14.04')
       return unless supported_os_list.include? current_os_version
       Chef::Application.fatal!("Current OS version is #{current_os_version}. " \
                                "Supported OS versions: #{supported_os_list.join(',')}")
@@ -97,6 +106,7 @@ module PreCheck
   # Network checks before update
   module HelperNetworkCheck
     def check_ip_reachable(ping_server_list)
+      #   Sample: check_ip_reachable(['www.google.com', 'www.github.com'])
       ping_server_list.each do |ping_server|
         ruby_block 'Check network connectivity' do
           block do
